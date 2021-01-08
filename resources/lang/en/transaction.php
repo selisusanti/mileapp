8dc03510dbf2c7326f9e69a15993442c3ca42746
seli <>
2021-01-08 09:14:50 +0700

    update dan get by id

---
+++ resources/lang/en/transaction.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'failure_update_transaction' => [
+        'code'          => 4100,
+        'status_code'   => 401,
+        'message'       => "Failed Insert"],
+    'failure_insert_transaction' => [
+        'code'          => 4101,
+        'status_code'   => 403,
+        'message'       => "Failed Update "],
+    'failure_delete_transaction' => [
+        'code'          => 4200,
+        'status_code'   => 422,
+        'message'       => "Failed Delete"],
+    'failure_select_transaction' => [
+        'code'          => 4200,
+        'status_code'   => 422,
+        'message'       => "Id :id not found."],
+
+];
